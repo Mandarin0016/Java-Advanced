65651da3d199fa0bc8d7ac70ef0317f890a419f9
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-05-27 23:47:37 +0300

    Sets And Maps Advanced - Exercises

---
+++ Sets And Maps Advanced - Exercises/src/SetsOfElements.java
@@ -0,0 +1,28 @@
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class SetsOfElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        int sizeFirstSet = Integer.parseInt(input.split("\\s+")[0]);
+        int sizeSecondSet = Integer.parseInt(input.split("\\s+")[1]);
+        Set<Integer> firstSet = new LinkedHashSet<>();
+        Set<Integer> secondSet = new LinkedHashSet<>();
+        for (int i = 1; i <= sizeFirstSet; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+            firstSet.add(number);
+        }
+        for (int i = 1; i <= sizeSecondSet; i++) {
+            secondSet.add(Integer.parseInt(scanner.nextLine()));
+        }
+        Set<Integer> duplicateElements = new LinkedHashSet<>();
+        for (int number : firstSet) {
+            if (secondSet.contains(number)) {
+                duplicateElements.add(number);
+            }
+        }
+        duplicateElements.forEach(el -> System.out.print(el + " "));
+    }
+}
