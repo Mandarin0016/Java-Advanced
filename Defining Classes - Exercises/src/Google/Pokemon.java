2a0fa7e8d27a2c5ef1880f6aa87f1de6b6592c65
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-06-07 17:43:26 +0300

    Defining Classes - Exercises

---
+++ Defining Classes - Exercises/src/Google/Pokemon.java
@@ -0,0 +1,28 @@
+package Google;
+
+public class Pokemon {
+    private String pokemonName;
+    private String pokemonType;
+
+    public Pokemon(String pokemonName, String pokemonType) {
+        this.pokemonName = pokemonName;
+        this.pokemonType = pokemonType;
+    }
+    @Override
+    public String toString(){
+        return String.format("%s %s", getPokemonName(), getPokemonType());
+    }
+
+    public String getPokemonName() {
+        return pokemonName;
+    }
+    public void setPokemonName(String pokemonName) {
+        this.pokemonName = pokemonName;
+    }
+    public String getPokemonType() {
+        return pokemonType;
+    }
+    public void setPokemonType(String pokemonType) {
+        this.pokemonType = pokemonType;
+    }
+}
