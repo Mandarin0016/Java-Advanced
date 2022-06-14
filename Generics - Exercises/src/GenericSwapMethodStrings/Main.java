e855632252ad7f423fb233d3a4541b0934775f40
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-06-14 20:24:47 +0300

    Generics - Exercises

---
+++ Generics - Exercises/src/GenericSwapMethodStrings/Main.java
@@ -0,0 +1,18 @@
+package GenericSwapMethodStrings;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Box<String> box = new Box<>();
+        for (int i = 0; i < n; i++) {
+            box.add(scanner.nextLine());
+        }
+        int indexOne = scanner.nextInt();
+        int indexTwo = scanner.nextInt();
+        Box.swap(box.getElements(), indexOne, indexTwo);
+        System.out.println(box);
+    }
+}
