ad941b22301f4a197e18d602c0cc183f610b64c3
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-06-15 10:59:02 +0300

    Iterators and Comparators - Lab

---
+++ Iterators and Comparators - Lab/src/BookComparator/BookComparator.java
@@ -0,0 +1,14 @@
+package BookComparator;
+
+import java.util.Comparator;
+
+public class BookComparator implements Comparator<Book> {
+    @Override
+    public int compare(Book firstBook, Book secondBook) {
+        int result = firstBook.getTitle().compareTo(secondBook.getTitle());
+        if (result == 0){
+             result = Integer.compare(firstBook.getYear(), secondBook.getYear());
+        }
+        return result;
+    }
+}
